1ed1a288944a19f96440996c929ec245c09e036f
Jali <jali@orca-central.de>
2021-04-11 18:26:14 +0200

    Added start code

---
+++ src/__init__.py
@@ -0,0 +1,34 @@
+import os
+
+from flask import Flask
+
+
+# functions
+def create_app(test_config=None):
+    """! Creates a new instance of the godparent app
+    @param test_config The configuration data for testing the app.
+    @return The application instance
+    """
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='debug',
+                            DATABASE=os.path.join(app.instance_path,
+                                                  'godparent.db'))
+    if test_config is None:
+        # load the instance config if it exists
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config, if pased in
+        app.config.from_pyfile(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # for starters, just say hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
