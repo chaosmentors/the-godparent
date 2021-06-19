86e19381f35f9082e572efb99f4a360e09a029d1
Jali <jali@orca-central.de>
2021-06-19 20:08:56 +0200

    New model for adding searchable tags to users

--- app/models/users.py
+++ app/models/users.py
@@ -2,6 +2,12 @@
 from flask_login import UserMixin
 from werkzeug.security import check_password_hash, generate_password_hash
 from app import db, login
+from app.models.tags import Tag
+
+user_tags = db.Table(
+    'user_tag', db.Model.metadata,
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')))
 
 
 class User(UserMixin, db.Model):
@@ -17,6 +23,7 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(128))
     pronoun = db.Column(db.String(50))
     role = db.Column(db.Integer, nullable=False, default=0)
+    tags = db.relationship('Tag', secondary='user_tags', backref='user')
 
     def __repr__(self):
         return '<User {}>'.format(self.nickname)
