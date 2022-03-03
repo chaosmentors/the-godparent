49868a6aa7188f05983d74719daa5d4eee8d00e7
Jali <jali@orca-central.de>
2022-03-03 19:58:38 +0100

    Add static pages table

--- app/forms/static.py
+++ app/forms/static.py
@@ -3,10 +3,11 @@
     and the privacy policy.
 """
 
-from flask_table import Table, Col, LinkCol, create_table
+from flask_table import Table, Col, LinkCol, OptCol, create_table
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
 from app.models.languages import Language
+from app.models.pages import PageTypeDescriptions
 
 class EditStaticForm(FlaskForm):
     """ Edit the content of a specific page/language """
@@ -14,13 +15,31 @@ class EditStaticForm(FlaskForm):
     description = StringField('Title', validators=None)
     submit = SubmitField('Save')
 
+
+class StaticPageItem:
+    """ An entry to the static page table """
+    def __init__(self, page_type, language_id):
+        self.page_type = page_type
+        self.language = language_id
+        self.edit = 'Edit'
+
+
 def create_static_table():
     """ Generates a table that contains the list of static pages and links
         to the editors per language
     """
-    languages = Language.query.order_by(Language.iso_code)
-    table = create_table('TableCls').add_column('page_type',Col('Page Type'))
-    for lang in languages:
-        table.add_column(lang.iso_code, LinkCol(lang.iso_code, 'staticpages.edit',
-                                                url_kwargs=dict(page_type='page_type',iso_code='iso_code')))
-    return table
+    languages = Language.query.order_by(Language.iso_code).all()
+    choices = {}
+    for language in languages:
+        choices[language.iso_code] = language.name
+
+    TableCls = create_table('TableCls')\
+        .add_column('page_type', Col('Page Type'))\
+        .add_column('language', OptCol('Language', choices))\
+        .add_column('edit', Col('Edit'))
+
+    items = []
+    for page_type in PageTypeDescriptions:
+        items.append(StaticPageItem(page_type,languages[0].iso_code))
+
+    return TableCls(items)
